bea2ec090386b66350425e90bfd7d58ff46fe025
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-02-02 04:09:31 +0000

    Factorial function with negative

--- FactorialLibrary/FactorialService.cs
+++ FactorialLibrary/FactorialService.cs
@@ -4,7 +4,9 @@ public class FactorialService
 {
     public static int Factorial(int n)
     {
-        if(n == 0 || n == 1){
+        if(n<0) {
+            throw new InvalidOperationException("Negative numbers don't have proper factorial");
+        } if(n == 0 || n == 1){
             return 1;
         } else{
             return n * Factorial(n-1);
